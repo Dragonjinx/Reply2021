7ee11baba3cef7278e66b3dfd581f9c747450aa6
Dragonjinx <abhilekh1@hotmail.com>
2021-03-11 18:38:09 +0100

    completed parsing from a file

--- main.cpp
+++ main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
+#include <fstream>
 #include <vector>
 #include <cstdlib>
+#include "structure.h"
 
-
-void parse_building(int &number)
+void parse_building(int &number, std::vector<Building*> &Buildings)
 {
     int counter = number-1;
     int Bx, By, Bl, Bc;
@@ -11,11 +12,13 @@ void parse_building(int &number)
     {
         std::cin >> Bx >> By >> Bl >> Bc;
         counter--;
+        Building * store = new Building(Bx, By, Bl, Bc);
+        Buildings.push_back(store);
     }
     //DO something with the structure
 }
 
-void parse_antenna(int &number)
+void parse_antenna(int &number, std::vector<Antenna*> &Antennas)
 {
     int counter = number-1;
     int Ar, Ac;
@@ -23,6 +26,8 @@ void parse_antenna(int &number)
     {
         std::cin >> Ar >> Ac;
         counter--;
+        Antenna * store = new Antenna(Ar, Ac);
+        Antennas.push_back(store);
     }
     //Do something with the structure
 }
@@ -30,14 +35,21 @@ void parse_antenna(int &number)
 
 int main(int Argc, char * Argv[])
 {
+
+    std::ifstream input_txt;
+    input_txt.open(Argv[1]);
+
     int grid_x, grid_y, Buildings_N, Antenna_M, Reward_R;
 
-    std::cin >> grid_x >> grid_y>> Buildings_N >> Antenna_M >> Reward_R;; 
+    input_txt >> grid_x >> grid_y>> Buildings_N >> Antenna_M >> Reward_R;; 
 
     // std::cout << "X" << grid_x << "Y" << grid_y << "N" << Buildings_N << "M" << Antenna_M << "R" << Reward_R;
-    
-    parse_building(Buildings_N);
-    parse_antenna(Antenna_M);
+
+    std::vector<Building *> Buildings;
+    std::vector<Antenna *> Antennas;
+
+    parse_building(Buildings_N, Buildings);
+    parse_antenna(Antenna_M, Antennas);
     
     return 0;
 }
