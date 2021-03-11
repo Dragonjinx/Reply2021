78b95aabfcf172917b89bbeaec604765fda81cee
Dragonjinx <abhilekh1@hotmail.com>
2021-03-11 17:59:15 +0100

    on linux now

--- main.cpp
+++ main.cpp
@@ -15,12 +15,11 @@ void parse(void)
 int main(int Argc, char * Argv[])
 {
     int grid_x, grid_y, Buildings_N, Antenna_M, Reward_R;
-    char newline_Dump;
 
-    std::cin >> grid_x >> grid_y >> newline_Dump;
-    std::cin >> Buildings_N >> Antenna_M >> Reward_R >> newline_Dump;
+    std::cin >> grid_x >> grid_y>> Buildings_N >> Antenna_M >> Reward_R;; 
 
-    std::cout << grid_y << grid_y << Buildings_N << Antenna_M << Reward_R;
+    std::cout << "X" << grid_x << "Y" << grid_y << "N" << Buildings_N << "M" << Antenna_M << "R" << Reward_R;
 
+    
     return 0;
 }
