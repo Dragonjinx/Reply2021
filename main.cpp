43139fe88f02fb972e76daff441c600f36b5dcf0
Dragonjinx <abhilekh1@hotmail.com>
2021-03-11 18:45:22 +0100

    Basic parsing works

--- main.cpp
+++ main.cpp
@@ -4,30 +4,32 @@
 #include <cstdlib>
 #include "structure.h"
 
-void parse_building(int &number, std::vector<Building*> &Buildings)
+void parse_building(int &number, std::vector<Building*> &Buildings, std::ifstream &input)
 {
     int counter = number-1;
     int Bx, By, Bl, Bc;
     while(counter != 0)
     {
-        std::cin >> Bx >> By >> Bl >> Bc;
+        input >> Bx >> By >> Bl >> Bc;
+        std::cout << Bx << By << Bl << Bc;
         counter--;
-        Building * store = new Building(Bx, By, Bl, Bc);
-        Buildings.push_back(store);
+        //Building * store = new Building(Bx, By, Bl, Bc);
+        //Buildings.push_back(store);
     }
     //DO something with the structure
 }
 
-void parse_antenna(int &number, std::vector<Antenna*> &Antennas)
+void parse_antenna(int &number, std::vector<Antenna*> &Antennas, std::ifstream &input)
 {
     int counter = number-1;
     int Ar, Ac;
     while (counter !=0)
     {
-        std::cin >> Ar >> Ac;
+        input >> Ar >> Ac;
+        std::cout << Ar << Ac;
         counter--;
-        Antenna * store = new Antenna(Ar, Ac);
-        Antennas.push_back(store);
+        //Antenna * store = new Antenna(Ar, Ac);
+        //Antennas.push_back(store);
     }
     //Do something with the structure
 }
@@ -37,19 +39,22 @@ int main(int Argc, char * Argv[])
 {
 
     std::ifstream input_txt;
+    std::cout << Argv[1] << "\n";
     input_txt.open(Argv[1]);
+    std::cout << "Nice\n";
 
     int grid_x, grid_y, Buildings_N, Antenna_M, Reward_R;
 
-    input_txt >> grid_x >> grid_y>> Buildings_N >> Antenna_M >> Reward_R;; 
+    input_txt >> grid_x >> grid_y>> Buildings_N >> Antenna_M >> Reward_R;
+    std::cout << grid_x <<grid_y << Buildings_N << Antenna_M << Reward_R;
 
     // std::cout << "X" << grid_x << "Y" << grid_y << "N" << Buildings_N << "M" << Antenna_M << "R" << Reward_R;
 
     std::vector<Building *> Buildings;
     std::vector<Antenna *> Antennas;
 
-    parse_building(Buildings_N, Buildings);
-    parse_antenna(Antenna_M, Antennas);
+    parse_building(Buildings_N, Buildings, input_txt);
+    parse_antenna(Antenna_M, Antennas, input_txt);
     
     return 0;
 }
